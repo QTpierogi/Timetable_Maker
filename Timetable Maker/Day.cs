4aa30c70c37990c32a0029539c8b59d5ea569923
cmxYZ <71211560+cmxYZ@users.noreply.github.com>
2021-12-06 12:33:22 +0500

    Add files via upload

---
+++ Timetable Maker/Day.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable_Maker
+{
+    //Служит элементом недели и хранит в taskList список задач в правильном (сгенерированном порядке)
+    public class Day
+    {
+        private List<Task> taskList { get; set; }
+
+        public Day(List<Task> taskList)
+        {
+            this.taskList = taskList;
+        }
+
+        public void AddTask(Task task)
+        {
+            taskList.Add(task);
+        }
+
+        public void RemoveTask(Task task)
+        {
+            taskList.Remove(task);
+        }
+    }
+}
